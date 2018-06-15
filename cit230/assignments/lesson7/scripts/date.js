0083c2a584ea0b8a547ec2500877bf4748b2cf19
thenoblehatch26 <38664443+thenoblehatch26@users.noreply.github.com>
2018-06-15 13:34:41 -0600

    lots

---
+++ cit230/assignments/lesson7/scripts/date.js
@@ -0,0 +1,35 @@
+function whatDay(){ 
+    var n = new Date();
+    
+    var y = n.getFullYear();
+    
+    var month = new Array(12);
+    month[0] = "January";
+    month[1] = "February";
+    month[2] = "March";
+    month[3] = "April";
+    month[4] = "May";
+    month[5] = "June";
+    month[6] = "July";
+    month[7] = "August";
+    month[8] = "September";
+    month[9] = "October";
+    month[10] = "November";
+    month[11] = "December";
+    var m = month[n.getMonth()];
+    
+    var d = n.getDate();
+    
+    var weekday = new Array(7);
+    weekday[0] = "Sunday";
+    weekday[1] = "Monday";
+    weekday[2] = "Tuesday";
+    weekday[3] = "Wednesday";
+    weekday[4] = "Thursday";
+    weekday[5] = "Friday";
+    weekday[6] = "Saturday";
+
+    var a = weekday[n.getDay()];
+    
+    document.getElementById("currentdate").innerHTML = a + ", " + d + "/" + m + y;  
+}
