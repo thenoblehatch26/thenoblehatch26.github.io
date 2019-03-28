ef33f178800db00b50a1f96ba2b2671a29ddc245
thenoblehatch26 <chandlerhatch26@gmail.com>
2019-03-28 17:48:55 -0600

    wow lotz

--- cit230/assignments/weathersite/scripts/home.js
+++ cit230/assignments/weathersite/scripts/home.js
@@ -1,22 +1,3 @@
-var section = document.querySelector('section');
-
-
-var requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
-var request = new XMLHttpRequest();
-
-request.open('GET', requestURL);
-
-request.responseType = 'json';
-request.send();
-
-var townName = JSON.parse(requestURL);
-
-document.getElementsById('preston').innerHTML = townName.towns;
-
-
-
-
-
 ///////////////////////////////////////////////////////////////////////////////////////
 
 
@@ -29,19 +10,54 @@ weatherObject.onload = function () {
     var currentForcast = JSON.parse(weatherObject.responseText);
     console.log(currentForcast);
 
-    document.getElementById('currenttemp').innerHTML = currentForcast.main.temp;
-    document.getElementById('windSpeed').innerHTML = currentForcast.wind.speed;
-    document.getElementById('high').innerHTML = currentForcast.main.temp_max;
-    document.getElementById('low').innerHTML = currentForcast.main.temp_min;
-    document.getElementById('windChill').innerHTML = currentForcast.weather["0"].main;
+    document.getElementById('preston-currenttemp').innerHTML = currentForcast.main.temp;
+    document.getElementById('preston-windSpeed').innerHTML = currentForcast.wind.speed;
+    document.getElementById('preston-windChill').innerHTML = currentForcast.weather["0"].main;
 //                                                  ||
 //                                                  ||
 //                                                  \/  this is how you call a variable in another object!! 
     var windChill = 35.74 + 0.6215 * currentForcast.main.temp - 35.75 * Math.pow(currentForcast.wind.speed, 0.16) + 0.4275 * currentForcast.main.temp * Math.pow(currentForcast.wind.speed, 0.16);
 
-    document.getElementById("windchill").innerHTML = windChill.toFixed(2);
+    document.getElementById("preston-windChill").innerHTML = windChill.toFixed(2);
 
-    var iconcode = currentForcast.weather[0].icon;
-    var icon_path = "//openweathermap.org/img/w/" + iconcode + ".png";
-    document.getElementById('weather_icon').src = icon_path;
+}
+
+var weatherObject = new XMLHttpRequest();
+weatherObject.open('GET', 'https://api.openweathermap.org/data/2.5/weather?id=5678757&appid=63d7b710055757e4b59628d0938f81f3&units=imperial', true)
+weatherObject.send();
+
+weatherObject.onload = function () {
+    var currentForcast = JSON.parse(weatherObject.responseText);
+    console.log(currentForcast);
+
+    document.getElementById('sodasprings-currenttemp').innerHTML = currentForcast.main.temp;
+    document.getElementById('sodasprings-windSpeed').innerHTML = currentForcast.wind.speed;
+    document.getElementById('sodasprings-windChill').innerHTML = currentForcast.weather["0"].main;
+//                                                  ||
+//                                                  ||
+//                                                  \/  this is how you call a variable in another object!! 
+    var windChill = 35.74 + 0.6215 * currentForcast.main.temp - 35.75 * Math.pow(currentForcast.wind.speed, 0.16) + 0.4275 * currentForcast.main.temp * Math.pow(currentForcast.wind.speed, 0.16);
+
+    document.getElementById("sodasprings-windChill").innerHTML = windChill.toFixed(2);
+
+}
+
+var weatherObject = new XMLHttpRequest();
+weatherObject.open('GET', 'https://api.openweathermap.org/data/2.5/weather?id=5585000&appid=63d7b710055757e4b59628d0938f81f3&units=imperial', true)
+weatherObject.send();
+
+weatherObject.onload = function () {
+    var currentForcast = JSON.parse(weatherObject.responseText);
+    console.log(currentForcast);
+
+    document.getElementById('fishhaven-currenttemp').innerHTML = currentForcast.main.temp;
+    document.getElementById('fishhaven-windSpeed').innerHTML = currentForcast.wind.speed;
+    document.getElementById('fishhaven-windChill').innerHTML = currentForcast.weather["0"].main;
+//                                                  ||
+//                                                  ||
+//                                                  \/  this is how you call a variable in another object!! 
+    var windChill = 35.74 + 0.6215 * currentForcast.main.temp - 35.75 * Math.pow(currentForcast.wind.speed, 0.16) + 0.4275 * currentForcast.main.temp * Math.pow(currentForcast.wind.speed, 0.16);
+
+    document.getElementById("fishhaven-windChill").innerHTML = windChill.toFixed(2);
+
 }
