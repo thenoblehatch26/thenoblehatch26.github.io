fc7369d8a3abb0ef766c4c824b3f3c4ce2223c02
thenoblehatch26 <chandlerhatch26@gmail.com>
2019-04-10 11:03:16 -0600

    sdf

--- cit230/assignments/final/scripts/loganjson.js
+++ cit230/assignments/final/scripts/loganjson.js
@@ -1,13 +1,12 @@
 var loganData = new XMLHttpRequest();
-loganData.open('GET', 'https://thenoblehatch26.github.io/cit230/assignments/final/json/temples.json', true)
+loganData.open('GET', 'https://thenoblehatch26.github.io/cit230/assignments/final/json/temples.json')
 loganData.send();
 
-loganData.onload = function () {
+
     var loganServices = JSON.stringify(loganData);
     console.log(loganServices);
     console.log("this is a test");
+    console.log(loganData[0].name);
 
-
-document.getElementById('service1').innerHTML = loganServices[1].name;
+document.getElementById('service1').innerHTML = loganServices;
 document.getElementById("service2").innerHTML = loganData[2].name;
-}
