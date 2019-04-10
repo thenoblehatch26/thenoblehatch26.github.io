620e3eba0c745a81668bb2a2a3ca81b41b34c123
thenoblehatch26 <chandlerhatch26@gmail.com>
2019-04-10 10:49:38 -0600

    hope

--- cit230/assignments/final/scripts/loganjson.js
+++ cit230/assignments/final/scripts/loganjson.js
@@ -8,6 +8,6 @@ loganData.onload = function () {
     console.log("this is a test");
 
 
-document.getElementById('service1').innerHTML = loganServices;
-document.getElementById("service2").innerHTML = loganServices.services["2"].name["2"];
+document.getElementById('service1').innerHTML = loganServices[1].name;
+document.getElementById("service2").innerHTML = loganData[2].name;
 }
